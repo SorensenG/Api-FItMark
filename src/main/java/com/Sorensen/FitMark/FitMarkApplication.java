59d3ee1213a94b0ceb380daeea2045fd2b6783c3
SorensenG <g.soren.sen2004@gmail.com>
2026-01-23 16:54:26 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/Sorensen/FitMark/FitMarkApplication.java
@@ -0,0 +1,13 @@
+package com.Sorensen.FitMark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitMarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitMarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Sorensen/FitMark/FitMarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sorensen.FitMark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitMarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
