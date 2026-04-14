569a05f3e8f025231902202c47718fa4adaacd8e
Gabriel Sorensen <g.soren.sen2004@gmail.com>
2026-04-13 21:33:44 -0300

    feat(auth): add forgot password with 6-digit code verification

--- src/main/java/com/Sorensen/FitMark/config/MailConfig.java
+++ src/main/java/com/Sorensen/FitMark/config/MailConfig.java
@@ -11,23 +11,14 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
-    @Value("${spring.mail.host}")
-    private String host;
-
-    @Value("${spring.mail.port}")
-    private int port;
-
-    @Value("${spring.mail.username}")
-    private String username;
-
-    @Value("${spring.mail.password}")
-    private String password;
-
     @Bean
-    public JavaMailSender javaMailSender() {
+    public JavaMailSender javaMailSender(
+            @Value("${MAIL_USERNAME}") String username,
+            @Value("${MAIL_PASSWORD}") String password) {
+
         JavaMailSenderImpl sender = new JavaMailSenderImpl();
-        sender.setHost(host);
-        sender.setPort(port);
+        sender.setHost("smtp.gmail.com");
+        sender.setPort(587);
         sender.setUsername(username);
         sender.setPassword(password);
 
